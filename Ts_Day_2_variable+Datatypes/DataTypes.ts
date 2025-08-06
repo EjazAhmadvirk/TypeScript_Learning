cc186c842d874a4a27eb0d550b2f1fdfba38c7bc
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-06 07:35:58 +0500

    new comments add

--- Ts_Day_2_variable+Datatypes/DataTypes.ts
+++ Ts_Day_2_variable+Datatypes/DataTypes.ts
@@ -9,3 +9,10 @@ console.log(`User: ${user.name}, Age: ${user.age}, Active: ${isActive}`);
 console.log(name, age, items);
 console.log(`Items: ${items.join(", ")}`);
 console.log(`Is Active: ${isActive}`);
+
+
+
+// for output link this file with HTML file
+// and run in browser console to see the output
+// for link use <script type="module" src="DataTypes.ts"></script>
+// This code demonstrates basic TypeScript data types and their usage
