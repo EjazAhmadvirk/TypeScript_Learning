66c16cc23044c96e471e0d6005e3fef10143ff88
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-07 09:46:34 +0500

    do while loop file

---
+++ Ts_Day_3_input+Loops/do_while_loop.ts
@@ -0,0 +1,6 @@
+let n: number = 0;
+do {
+    console.log(`Current value of n: ${n}`);
+    n++;
+} while (n < 5);
+// This code will execute at least once, even if the condition is false initially.
