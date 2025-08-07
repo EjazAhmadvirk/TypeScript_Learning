55e5f4e81e5f79d2c16f9d52e0436a2360ea9aad
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-07 09:54:38 +0500

    for loop files

---
+++ Ts_Day_3_input+Loops/for_loop.ts
@@ -0,0 +1,8 @@
+for(let i: number = 0; i < 5; i++) {
+    console.log(`Current value of i: ${i}`);
+}   
+// This code will execute as long as the condition is true, and it will not execute if the condition is false initially.
+
+// if you want to see output, you can run this code in a TypeScript environment or a browser console.
+// you can also use HTML to create a simple interface to run this code, but for now, this is a basic example of a for loop in TypeScript.
+// for HTML you write this code for connect TypeScript file
