cd17a1eb8c533771bb08e07c224a5d738bc09f40
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-08 23:09:02 +0500

    new files

---
+++ Ts_Day_4_if_else+switch/nested_if_else.ts
@@ -0,0 +1,10 @@
+let amount: number = 1500;
+if(amount >= 2000) {
+    if(amount >= 5000) {
+        console.log("You are eligible for a premium account.");
+    } else {
+        console.log("You are eligible for a standard account.");
+    }
+else {
+    console.log("You are not eligible for any account.");
+}
---
+++ Ts_Day_4_if_else+switch/switch.ts
@@ -0,0 +1,14 @@
+let grade: string = "B";
+switch(grade) {
+    case "A":
+        console.log("Excellent!");
+        break;
+    case "B":
+        console.log("Good job!");
+        break;
+    case "C":
+        console.log("You can do better.");
+        break;
+    default:
+        console.log("Keep trying!");
+}
