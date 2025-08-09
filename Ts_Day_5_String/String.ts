4a9248c182e6ace5264d30c041ce2a44aa75a637
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-09 22:36:16 +0500

    new files

---
+++ Ts_Day_5_String/String.ts
@@ -0,0 +1,10 @@
+// Explicit type annotation
+let message: string = "Hello, TypeScript!";
+
+// Type inference (TS automatically infers string type)
+let greeting = "Hi there!";
+
+// Using single quotes, double quotes, or template literals
+let singleQuote: string = 'Hello';
+let doubleQuote: string = "World";
+let templateLiteral: string = `Hi, ${singleQuote} ${doubleQuote}!`;
