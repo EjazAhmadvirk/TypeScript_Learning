2e117e6e4977e8b9a0a2d1684cbcb56241ff7943
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-11 22:23:51 +0500

    adding

--- Ts_Day_7_TypeScript_Data_Satructure/Str_Map.ts
+++ Ts_Day_7_TypeScript_Data_Satructure/Str_Map.ts
@@ -1,5 +1,12 @@
 let myMap: Map<string, string> = new Map();
-myMap.set("name", "Ejaz");
+myMap.set("name", "Ejaz"); // key, value        
 myMap.set("country", "Pakistan");
 console.log(myMap);
-console.log(myMap.get("name"));
+console.log(myMap.get("name")); // get the value of the key
+
+// Iterate over the map
+for (let [key, value] of myMap) {
+    console.log(key, value);
+}
+
+// Iterate over the map using forEach
