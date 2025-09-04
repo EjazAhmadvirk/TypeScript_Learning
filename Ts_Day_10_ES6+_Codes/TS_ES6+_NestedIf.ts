fce941a25ec540ecfae26057355ea96c78d63f03
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-09-04 10:39:14 +0500

    nested if code

---
+++ Ts_Day_10_ES6+_Codes/TS_ES6+_NestedIf.ts
@@ -0,0 +1,12 @@
+let username: string = "Ejaz";
+let password: string = "12345";
+
+if (username === "Ejaz") {
+    if (password === "12345") {
+        console.log("Login successful!");
+    } else {
+        console.log("Incorrect password.");
+    }
+} else {
+    console.log("User not found.");
+}
