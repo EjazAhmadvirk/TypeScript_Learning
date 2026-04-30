e1805838d7a220b4a9794507951bb26a5a4ed8af
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2026-05-01 02:33:45 +0500

    Correct age condition for minor and adult
    Fix conditional logic for age check.

--- Ts_Day_4_if_else+switch/if_else.ts
+++ Ts_Day_4_if_else+switch/if_else.ts
@@ -1,6 +1,8 @@
 let age: number = 18;
+// if age is gratter then 18 then if condition 
 if(age<18) {
     console.log("You are a minor.");
+// if age is less then 18 then this condition 
 }else {
     console.log("You are an adult.");
 }
